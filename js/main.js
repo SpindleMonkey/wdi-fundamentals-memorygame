d096c83851c5519fffdce11feca788330deb81ff
connie kephart <spindle.monkey@gmail.com>
2017-06-15 14:28:01 -0600

    added minimal game logic

--- js/main.js
+++ js/main.js
@@ -1,9 +1,17 @@
-console.log("Up and running!");
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[1];
+var cardTwo = cards[3];
 
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
+cardsInPlay.push(cardOne);
+console.log("User flipped " + cardsInPlay[cardsInPlay.length-1]);
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardsInPlay[cardsInPlay.length-1]);
 
-console.log("User flipped " + cardTwo);
-console.log("User flipped " + cardThree);
+if (cardsInPlay.length === 2) {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    alert("You found a match!");
+  } else {
+    alert("Sorry, try again.");
+  }
+}
